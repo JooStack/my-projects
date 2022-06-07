4834a259f9ea2ddf7829c900cdadb8f2367bfa30
JooStack <joostack@gmail.com>
2022-06-07 21:48:17 +0300

    updated

--- python/hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ python/hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'PLEASE WRITE YOUR DATABASE ENDPOINT HERE'
+app.config['MYSQL_DATABASE_HOST'] = 'jose-mysql.cfolbjhqzdym.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
@@ -30,12 +30,14 @@ CREATE TABLE users (
 """
 data = """
 INSERT INTO clarusway.users 
-VALUES 
-	("Tuba", "tuba@amazon.com" ),
-	("Ethan", "ethan@micrasoft.com"),
-	("mostafa", "mostafa@facebook.com"),
-    ("sait", "sait@tesla.com"),
-    ("busra","busra@google");
+VALUES
+    ("Yusuf", "yusuf@amazon.com"),
+    ("Betul", "betul@google.com"),
+    ("Sparkle", "sparkle@bmw.com"),
+    ("Yildiz", "yildiz@mercedes.com"),
+	("Mehmet", "mehmet@porche.com"),
+    ("Tugba", "tugba@tesla.com"),
+    ("Kemal", "kemal@samsung.com");
 """
 cursor.execute(drop_table)
 cursor.execute(users_table)
@@ -107,6 +109,5 @@ def add_email():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-   app.run(debug=True)
-   # app.run(host='0.0.0.0', port=80)
-   app.run(host="0.0.0.0", port=80)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=80)
